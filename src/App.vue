4c7613860621911841c07ad1d8af61d701bc05da
CaptainEboy <emaapps.ac24@gmail.com>
2024-07-07 07:43:45 +0100

    Added the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,39 @@
+<!-- //App.vue -->
+
 <template>
+  <div id="app" class="container">
+    <h1>To-Do Application using Vue.js</h1>
+    <!-- <MyTodoForm /> // using MyTodoForm component -->
+    <MyTodoForm />
+    <!-- <MyTodoList /> // using MyTodoList component -->
+    <MyTodoList />
+
+  </div>
+</template>
+<script>
+
+import MyTodoList from './components/MyTodoList';
+import MyTodoForm from './components/MyTodoForm';
+
+export default {
+  name: 'App',
+  components: {
+    MyTodoList, // renders the MyTodoList component
+    MyTodoForm, // renders the MyTodoForm component
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<!-- <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
@@ -23,4 +58,4 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-</style>
+</style> -->
